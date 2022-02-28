d75c22ab4b6217e808a738589a21f08460fc4610
Hamid Zandi <hzandi1989@gmail.com>
2022-03-01 02:37:59 +0330

    change blog fragment UI

--- app/src/main/java/com/truecaller/truefoo/blog/view/BlogFragment.kt
+++ app/src/main/java/com/truecaller/truefoo/blog/view/BlogFragment.kt
@@ -11,7 +11,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.material.snackbar.Snackbar
 import com.truecaller.truefoo.R
+import com.truecaller.truefoo.blog.model.BlogModel
 import com.truecaller.truefoo.blog.viewmodel.BlogViewModel
+import com.truecaller.truefoo.common.BlogContentType.*
 import com.truecaller.truefoo.common.ViewState
 import com.truecaller.truefoo.databinding.FragmentBlogBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -39,54 +41,169 @@ class BlogFragment: Fragment() {
     private fun setupViews() {
         binding.fetchCardview.setOnClickListener {
             blogViewModel.getTenthCharacterBlogPost()
+            blogViewModel.getEveryTenthCharacterBlogPost()
+            blogViewModel.getWordCounterBlogPost()
+        }
+
+        binding.tenthCharTitleTextView.setOnClickListener {
+            toggleTenthCharContent()
+        }
+
+        binding.everyTenthCharTitleTextView.setOnClickListener {
+            toggleEveryTenthCharContent()
+        }
+
+        binding.wordCounterTitleTextView.setOnClickListener {
+            toggleWordCounterContent()
         }
     }
 
     private fun observeViewModel() {
         blogViewModel.tenthCharacterLiveData
-            .observe(viewLifecycleOwner) { viewState: ViewState<String> ->
-                when (viewState) {
-                    is ViewState.LoadingState -> showLoading()
-                    is ViewState.DataLoadedState -> handleDataLoadedState(viewState)
-                    is ViewState.ErrorState -> handleErrorState(viewState)
+            .observe(viewLifecycleOwner) { blogModel: BlogModel<ViewState<String>> ->
+                when (blogModel.viewState) {
+                    is ViewState.LoadingState -> showLoading(blogModel)
+                    is ViewState.DataLoadedState -> handleDataLoadedState(blogModel)
+                    is ViewState.ErrorState -> handleErrorState(blogModel)
                 }
             }
 
         blogViewModel.everyTenthCharacterLiveData
-            .observe(viewLifecycleOwner) { viewState: ViewState<String> ->
-                when (viewState) {
-                    is ViewState.LoadingState -> showLoading()
-                    is ViewState.DataLoadedState -> handleDataLoadedState(viewState)
-                    is ViewState.ErrorState -> handleErrorState(viewState)
+            .observe(viewLifecycleOwner) { blogModel: BlogModel<ViewState<String>> ->
+                when (blogModel.viewState) {
+                    is ViewState.LoadingState -> showLoading(blogModel)
+                    is ViewState.DataLoadedState -> handleDataLoadedState(blogModel)
+                    is ViewState.ErrorState -> handleErrorState(blogModel)
                 }
             }
 
         blogViewModel.wordCounterLiveData
-            .observe(viewLifecycleOwner) { viewState: ViewState<String> ->
-                when (viewState) {
-                    is ViewState.LoadingState -> showLoading()
-                    is ViewState.DataLoadedState -> handleDataLoadedState(viewState)
-                    is ViewState.ErrorState -> handleErrorState(viewState)
+            .observe(viewLifecycleOwner) { blogModel: BlogModel<ViewState<String>> ->
+                when (blogModel.viewState) {
+                    is ViewState.LoadingState -> showLoading(blogModel)
+                    is ViewState.DataLoadedState -> handleDataLoadedState(blogModel)
+                    is ViewState.ErrorState -> handleErrorState(blogModel)
                 }
             }
     }
 
-    private fun showLoading(){
-        binding.progressBar.isVisible = true
+    private fun showLoading(blogModel: BlogModel<ViewState<String>>){
+        when (blogModel.type) {
+            TENTH_CHAR -> {
+                binding.tenthCharProgressBar.isVisible = true
+                binding.tenthCharSuccessImageView.isGone = true
+                binding.tenthCharMeasuredTimeTextView.isGone = true
+            }
+            EVERY_TENTH_CHAR -> {
+                binding.everyTenthCharProgressBar.isVisible = true
+                binding.everyTenthCharSuccessImageView.isGone = true
+                binding.everyTenthCharMeasuredTimeTextView.isGone = true
+            }
+            WORD_COUNTER -> {
+                binding.wordCounterProgressBar.isVisible = true
+                binding.wordCounterSuccessImageView.isGone = true
+                binding.wordCounterMeasuredTimeTextView.isGone = true
+            }
+        }
     }
 
-    private fun handleDataLoadedState(viewState: ViewState.DataLoadedState<String>){
-        binding.progressBar.isGone = true
-        val blogContent = viewState.data
-        binding.contentTextView.text = blogContent
+    private fun handleDataLoadedState(blogModel: BlogModel<ViewState<String>>){
+        when (blogModel.type) {
+            TENTH_CHAR -> {
+                binding.tenthCharProgressBar.isGone = true
+                binding.tenthCharSuccessImageView.isVisible = true
+                binding.tenthCharArrowDownImageView.isVisible = true
+                binding.tenthCharArrowDownImageView.scaleY = 1f
+                binding.tenthCharContentTextView.text = blogModel.value
+                binding.tenthCharMeasuredTimeTextView.isVisible = true
+                val measuredTime = blogModel.measuredTime.toString() + " ms"
+                binding.tenthCharMeasuredTimeTextView.text = measuredTime
+            }
+            EVERY_TENTH_CHAR -> {
+                binding.everyTenthCharProgressBar.isGone = true
+                binding.everyTenthCharSuccessImageView.isVisible = true
+                binding.everyTenthCharArrowDownImageView.isVisible = true
+                binding.everyTenthCharArrowDownImageView.scaleY = 1f
+                binding.everyTenthCharContentTextView.text = blogModel.value
+                binding.everyTenthCharMeasuredTimeTextView.isVisible = true
+                val measuredTime = blogModel.measuredTime.toString() + " ms"
+                binding.everyTenthCharMeasuredTimeTextView.text = measuredTime
+            }
+            WORD_COUNTER -> {
+                binding.wordCounterProgressBar.isGone = true
+                binding.wordCounterSuccessImageView.isVisible = true
+                binding.wordCounterArrowDownImageView.isVisible = true
+                binding.wordCounterArrowDownImageView.scaleY = 1f
+                binding.wordCounterContentTextView.text = blogModel.value
+                binding.wordCounterMeasuredTimeTextView.isVisible = true
+                val measuredTime = blogModel.measuredTime.toString() + " ms"
+                binding.wordCounterMeasuredTimeTextView.text = measuredTime
+            }
+        }
     }
 
-    private fun handleErrorState(viewState: ViewState.ErrorState<String>) {
-        binding.progressBar.isGone = true
-        showSnackbar(viewState.errorMessage)
+    private fun handleErrorState(blogModel: BlogModel<ViewState<String>>) {
+        when (blogModel.type) {
+            TENTH_CHAR -> {
+                binding.tenthCharProgressBar.isGone = true
+                binding.tenthCharSuccessImageView.isGone = true
+                binding.tenthCharArrowDownImageView.isGone = true
+                binding.tenthCharMeasuredTimeTextView.isGone = true
+                blogModel.viewState?.let {
+                    showSnackbar((it as ViewState.ErrorState<String>).errorMessage)
+                }
+            }
+            EVERY_TENTH_CHAR -> {
+                binding.everyTenthCharProgressBar.isGone = true
+                binding.everyTenthCharSuccessImageView.isGone = true
+                binding.everyTenthCharArrowDownImageView.isGone = true
+                binding.everyTenthCharMeasuredTimeTextView.isGone = true
+                blogModel.viewState?.let {
+                    showSnackbar((it as ViewState.ErrorState<String>).errorMessage)
+                }
+            }
+            WORD_COUNTER -> {
+                binding.wordCounterProgressBar.isGone = true
+                binding.wordCounterSuccessImageView.isGone = true
+                binding.wordCounterArrowDownImageView.isGone = true
+                binding.wordCounterMeasuredTimeTextView.isGone = true
+                blogModel.viewState?.let {
+                    showSnackbar((it as ViewState.ErrorState<String>).errorMessage)
+                }
+            }
+        }
     }
 
-    @SuppressLint("ShowToast")
+    private fun toggleTenthCharContent(){
+        if(binding.tenthCharContentTextView.isVisible){
+            binding.tenthCharContentTextView.isGone = true
+            binding.tenthCharArrowDownImageView.scaleY = 1f
+        }else{
+            binding.tenthCharContentTextView.isVisible = true
+            binding.tenthCharArrowDownImageView.scaleY = -1f
+        }
+    }
+
+    private fun toggleEveryTenthCharContent(){
+        if(binding.everyTenthCharContentTextView.isVisible){
+            binding.everyTenthCharContentTextView.isGone = true
+            binding.everyTenthCharArrowDownImageView.scaleY = 1f
+        }else{
+            binding.everyTenthCharContentTextView.isVisible = true
+            binding.everyTenthCharArrowDownImageView.scaleY = -1f
+        }
+    }
+
+    private fun toggleWordCounterContent(){
+        if(binding.wordCounterContentTextView.isVisible){
+            binding.wordCounterContentTextView.isGone = true
+            binding.wordCounterArrowDownImageView.scaleY = 1f
+        }else{
+            binding.wordCounterContentTextView.isVisible = true
+            binding.wordCounterArrowDownImageView.scaleY = -1f
+        }
+    }
+
     private fun showSnackbar(message: String) {
         Snackbar.make(binding.root, message, Snackbar.LENGTH_INDEFINITE)
             .setAction(R.string.try_again) {
---
+++ app/src/main/java/com/truecaller/truefoo/common/BlogContentType.kt
@@ -0,0 +1,7 @@
+package com.truecaller.truefoo.common
+
+enum class BlogContentType {
+    TENTH_CHAR,
+    EVERY_TENTH_CHAR,
+    WORD_COUNTER
+}
